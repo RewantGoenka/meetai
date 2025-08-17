0a19d30795ae11a5e04f3e05bc144d0bb22a8716
Rewant <rewantgoenka87076@gmail.com>
2025-08-17 12:28:08 +0000

    03: authentication setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,66 @@
+"use client";
+
+import {Button} from "@/components/ui/button";
+import {Input} from "@/components/ui/input";
+import {useState} from "react";
+import { authClient } from "@/lib/auth-client";
+
+
 export default function Home() {
+  const { data: session} = authClient.useSession()
+
+  const [name,setName] = useState("");
+  const [email,setEmail] = useState("");
+  const[password,setPassword] = useState("");
+
+  const onSubmit = () => {
+    authClient.signUp.email({
+      email,
+      name,
+      password,
+    },{
+      onError: () => {
+        window.alert("Something went wrong");  
+      },
+      onSuccess: () => {
+        window.alert("Success")
+      }
+    });
+  }
+    const onLogin = () => {
+    authClient.signIn.email({
+      email,
+      password,
+    },{
+      onError: () => {
+        window.alert("Something went wrong");  
+      },
+      onSuccess: () => {
+        window.alert("Success")
+      }
+    });
+  }
   return (
-    <div>Hello World!</div>
-  );
-}
+    <>
+      <div className="p-4 flex flex-col gap-y-4">
+        <Input placeholder="name" value={name} onChange={(e) => setName(e.target.value)} />
+        <Input placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <Input placeholder="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </div>
+      <div className="p-4 flex flex-col gap-y-4">
+        <Button onClick={onSubmit}>
+          Create User
+        </Button>
+      </div>
+      <div className="p-4 flex flex-col gap-y-4">
+        <Input placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <Input placeholder="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </div>
+      <div className="p-4 flex flex-col gap-y-4">
+        <Button onClick={onLogin}>
+          Login
+        </Button>
+      </div>
+    </>
+    )
+  }
