5e16b97172b3fb4c3fb5d8ba24a603149dadfd15
Rewant Goenka <rewantgoenka87076@gmail.com>
2025-09-16 12:38:35 +0530

    09-agents-setup

---
+++ src/modules/home/ui/views/agents-view.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { ErrorState } from "@/components/error-state";
+import {  dataTagErrorSymbol, useSuspenseQuery } from "@tanstack/react-query";
+import { useTRPC } from "@/trpc/client";
+import { LoadingState } from "@/components/loadingstate";
+
+export const AgentsView = () => {
+  const trpc = useTRPC();
+  const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
+
+  return (
+    <div>
+      {JSON.stringify(data, null, 2)}
+    </div>
+  );
+};
+
+export const AgentsViewLoading = () => {
+  return (
+    <LoadingState
+      title="Loading agents"
+      description="Please wait while we load the agents."
+    />
+  );
+};
+
+export const AgentsViewError = () => {
+  return (
+    <ErrorState
+      title="Failed to load agents"
+      description="Please try again later."
+    />
+  );
+};
