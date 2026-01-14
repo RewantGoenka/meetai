26ea57a9c925af60719f5949eedda887162990fa
Rewant <rewantgoenka87076@gmail.com>
2026-01-14 16:56:27 +0000

    chnage3

--- src/app/api/stream/endcall/route.ts
+++ src/app/api/stream/endcall/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { streamVideo } from "@/lib/stream-video";
+export const dynamic = 'force-dynamic';
 
 export async function POST(req: NextRequest) {
   const { callId } = await req.json();
